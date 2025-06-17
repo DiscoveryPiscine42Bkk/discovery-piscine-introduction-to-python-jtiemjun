b55e190a43b8f2c8a8e615b4f9070545db345738
jtiemjun <pairmaizaza8@gmail.com>
2025-06-17 09:11:33 +0000

    done

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,5 +1,5 @@
 for multipiler in range (11):
     print(f"Table de {multipiler}:", end="")
     for number in range (11):
-        print(f"{multipiler*number}", end="")
+        print(f" {multipiler*number}", end="")
     print()
